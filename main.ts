51c7c4297d90bdd337c2e0a1953c2acaf006eb5c
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-24 20:31:57 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 function gelb (aNum: number) {
     basic.showString("Y")
-    pins.analogWritePin(AnalogPin.P1, dioden_helligkeit)
+    pins.analogWritePin(ampelseite[aNum]+1, dioden_helligkeit)
     basic.pause(warte_kurz)
 }
 function gruen (aNum: number) {
     basic.showString("G")
-    pins.analogWritePin(AnalogPin.P0, 0)
-    pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, dioden_helligkeit)
+    pins.analogWritePin(ampelseite[aNum]+0, 0)
+    pins.analogWritePin(ampelseite[aNum]+1, 0)
+    pins.analogWritePin(ampelseite[aNum]+2, dioden_helligkeit)
     basic.pause(warte_lang)
     for (let index = 0; index < 4; index++) {
-        pins.analogWritePin(AnalogPin.P2, 0)
+        pins.analogWritePin(ampelseite[aNum]+2, 0)
         basic.pause(warte_blink)
-        pins.analogWritePin(AnalogPin.P2, dioden_helligkeit)
+        pins.analogWritePin(ampelseite[aNum]+, dioden_helligkeit)
         basic.pause(warte_blink)
     }
     alle_aus(aNum)
@@ -21,19 +21,20 @@ function gruen (aNum: number) {
 function rot (aNum: number) {
     alle_aus(1)
     basic.showString("R")
-    pins.analogWritePin(AnalogPin.P0, dioden_helligkeit)
+    pins.analogWritePin(ampelseite[aNum]+0, dioden_helligkeit)
     basic.pause(warte_lang)
 }
 function alle_aus (aNum: number) {
-    pins.analogWritePin(AnalogPin.P0, 0)
-    pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
+    pins.analogWritePin(ampelseite[aNum]+0, 0)
+    pins.analogWritePin(ampelseite[aNum]+1, 0)
+    pins.analogWritePin(ampelseite[aNum]+2, 0)
 }
 let warte_blink = 0
 let warte_kurz = 0
 let warte_lang = 0
 let dioden_helligkeit = 0
 basic.showIcon(IconNames.Heart)
+let ampelseite = ["100", "112"]
 dioden_helligkeit = 512
 warte_lang = 5000
 warte_kurz = 2000
@@ -41,5 +42,5 @@ warte_blink = 500
 basic.forever(function () {
     rot(0)
     gelb(0)
-    gruen(1)
+    gruen(0)
 })
